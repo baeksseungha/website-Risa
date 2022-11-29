7a2f5944b8357e2fa3aaec1cd50c983b1c542774
webnautes <webnautes@nate.com>
2022-11-29 15:54:49 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex flex-col">
+      <div className="bg-red-300 m-3 text-center text-2xl mt-">red</div>
+      <div className="bg-red-300 m-3 text-center text-2xl mt-">red</div>
+      <div className="bg-orange-300 m-3 text-center text-2xl ">red</div>
+      <div className="bg-green-300 m-3 text-center text-2xl ">red</div>
     </div>
   );
 }
-
-export default App;
