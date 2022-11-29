6be8f4c8af873510b0ca56da00ece4170696d70a
webnautes <webnautes@nate.com>
2022-11-29 16:27:43 +0900

    make risa first page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 export default function App() {
   return (
     <div className="flex flex-col">
-      <div className="bg-red-300 m-3 text-center text-2xl mt-">red</div>
-      <div className="bg-red-300 m-3 text-center text-2xl mt-">red</div>
-      <div className="bg-orange-300 m-3 text-center text-2xl ">red</div>
-      <div className="bg-green-300 m-3 text-center text-2xl ">red</div>
+      <div className="m-3 text-center text-7xl">Risa</div>
+      <div className="bg-white-300 m-4 pl-3 text-2xl mt-40">Email</div>
+      <div className="bg-red-300 m-4 text-center text-2xl mt-">red</div>
+      <div className="bg-orange-300 m-4 text-center text-2xl ">red</div>
+      <div className="bg-green-300 m-4 text-center text-2xl ">red</div>
     </div>
   );
 }
